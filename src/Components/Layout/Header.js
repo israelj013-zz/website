55b8e4e2af0c8c646a8f571af4f661ce0197e844
israeljimenez <israelj@uci.edu>
2020-04-04 18:39:07 -0700

    updating webpage links

--- src/Components/Layout/Header.js
+++ src/Components/Layout/Header.js
@@ -8,18 +8,18 @@ function Header() {
     return (
         <Navbar bg="light" expand="lg">
 
-            <Navbar.Brand href="/">
+            <Navbar.Brand href="/website">
                 <img classname="logo" src={Logo} alt="logo" height="50"/>
             </Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="ml-auto">
-                <NavDropdown title="Projects" href="/projects" id="basic-nav-dropdown">
+                <NavDropdown title="Projects" href="/website/projects" id="basic-nav-dropdown">
                     <NavDropdown.Item href="#action/3.1">Project</NavDropdown.Item>
                     <NavDropdown.Item href="#action/3.2">Another Project</NavDropdown.Item>
                     <NavDropdown.Item href="#action/3.3">Some Other Project</NavDropdown.Item>
                 </NavDropdown>
-                <Nav.Link href="/projects">Bio</Nav.Link>
+                <Nav.Link href="/website/projects">Bio</Nav.Link>
                 <Nav.Link href="#link">Contact</Nav.Link>
                 
                 </Nav>
